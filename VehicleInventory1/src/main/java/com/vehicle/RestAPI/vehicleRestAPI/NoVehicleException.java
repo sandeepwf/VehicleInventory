10c44ecc17b93d88edf69a3ce1079af4c4a0b68d
sandeep <gsandeepfsd@gmail.com>
2018-06-29 15:17:50 -0500

    Initial commit

---
+++ VehicleInventory1/src/main/java/com/vehicle/RestAPI/vehicleRestAPI/NoVehicleException.java
@@ -0,0 +1,13 @@
+package com.vehicle.RestAPI.vehicleRestAPI;
+
+
+@SuppressWarnings("serial")
+public class NoVehicleException extends RuntimeException {
+
+
+	public NoVehicleException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
---
+++ VehicleInventory1/src/main/java/com/vehicle/RestAPI/vehicleRestAPI/VehicleRestAPIApplication.java
@@ -0,0 +1,12 @@
+package com.vehicle.RestAPI.vehicleRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRestAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRestAPIApplication.class, args);
+	}
+}
